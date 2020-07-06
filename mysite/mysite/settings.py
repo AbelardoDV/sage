a2d8bf69d6936c08e0d0935a9d5c2d843655b704
Diaz Rodolfo <rodolfo.diaz@nokia.com>
2020-07-06 18:18:29 -0500

    hola

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -80,6 +80,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'spatial': {
+        'NAME': 'Practicando',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': 'nemuuser',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
